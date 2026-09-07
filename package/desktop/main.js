f76dd2aa0caaa0103cd9860999f52371d0543fe1
BIGJOEBusinessAI <joshuachinenye6@gmail.com>
2026-09-07 11:57:59 +0100

    Update main.js

--- package/desktop/main.js
+++ package/desktop/main.js
@@ -55,6 +55,7 @@ function createWindow() {
 
 app.whenReady().then(() => {
   Menu.setApplicationMenu(null);
+ autoUpdater.checkForUpdatesAndNotify();
   createWindow();
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
